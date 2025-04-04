47ae92ac30b23f90eb4e3080a376c18dfa416b56
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-04-04 12:31:33 -0400

    setting up Mantine and disable TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css"; // Import Mantine styles
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
