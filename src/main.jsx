abee1e4ad67942dbc31495300d8ce738def1df4b
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-04-04 12:39:01 -0400

    unset mantine and reimplement tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css"; // Import Mantine styles
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <App />
   </StrictMode>
 );
