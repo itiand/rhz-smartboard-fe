725def722b4c81e99d33e9b9edc85ad482b4fec8
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-04-04 13:29:49 -0400

    start Smartboard grid component

---
+++ src/components/SmarboardGrid/SmartboardGrid.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SmartboardGrid.css";
+
+const SmartboardGrid = () => {
+  return <div className="smartboard-grid">SmartboardGrid</div>;
+};
+
+export default SmartboardGrid;
