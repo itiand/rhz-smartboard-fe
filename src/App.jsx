1b4500fc4a1c4f690d032c4faf3841e718817b40
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-04-07 21:03:46 -0400

    setting up initial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import {BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
+import SinglePostDetail from "./pages/SinglePostDetail";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
-    <>
+    <BrowserRouter>
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold underline">Smartboard</h1>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:postId" element={<SinglePostDetail />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
