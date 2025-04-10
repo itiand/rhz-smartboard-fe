e9a9305a749fe1e01967d650a0ec3e7b3665accd
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-04-10 17:45:17 -0400

    start building EditSmartBoard component

---
+++ src/components/EditSmartboardForm/EditSmartboardForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditSmartboardForm = () => {
+  return <div>EditSmartboardForm</div>;
+};
+
+export default EditSmartboardForm;
--- src/pages/Components/Components.jsx
+++ src/pages/Components/Components.jsx
@@ -1,6 +1,5 @@
 import FeedCard from "../../components/FeedCard/FeedCard";
-
-FeedCard;
+import EditSmartboardForm from "../../components/EditSmartboardForm/EditSmartboardForm";
 
 const Components = () => {
   const feedCards = [
@@ -13,12 +12,20 @@ const Components = () => {
 
   return (
     <div className="container grey-border flex flex-col gap-2 justify-center items-center h-screen">
-      <h1>Components</h1>
-      <FeedCard
-        title={feedCards[0].title}
-        category={feedCards[0].category}
-        image={feedCards[0].image}
-      />
+      <div className="component-list flex flex-col gap-6">
+        <div className="component-list-item">
+          <h1 className="text-xl font-bold">Edit Smartboard Form</h1>
+          <EditSmartboardForm />
+        </div>
+        <div className="component-list-item">
+          <h1 className="text-xl font-bold">Feed Card</h1>
+          <FeedCard
+            title={feedCards[0].title}
+            category={feedCards[0].category}
+            image={feedCards[0].image}
+          />
+        </div>
+      </div>
     </div>
   );
 };
